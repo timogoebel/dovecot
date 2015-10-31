5dbf19e81bd3ab9158f46389f6326dcc4188b8e4
Timo Goebel <mail@timogoebel.name>
2015-10-31 19:02:01 +0100

    improve sieve support

--- manifests/lda.pp
+++ manifests/lda.pp
@@ -1,6 +1,7 @@
 # 15-lda.conf
 class dovecot::lda (
   $postmaster_address = "root@${::fqdn}",
+  $mail_plugins       = '$mail_plugins',
 ) {
   include dovecot
 
@@ -8,7 +9,7 @@ class dovecot::lda (
     config_file => 'conf.d/15-lda.conf',
     changes     => [
       "set postmaster_address '${postmaster_address}'",
-      "set protocol[ . = 'lda']/mail_plugins '\$mail_plugins'",
+      "set protocol[ . = 'lda']/mail_plugins \"${mail_plugins}\"",
     ],
   }
 }
